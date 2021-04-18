1f21a1e86fb2581d7d4f61506b0febedc19294ab
apolorossi <apolorossi@gmail.com>
2021-04-18 12:54:17 -0300

    feat: Creating a blocking network call

--- app/src/main/java/com/rossi/coroutineslab/MainActivity.kt
+++ app/src/main/java/com/rossi/coroutineslab/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.rossi.coroutineslab
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val url = URL("https://www.stickaz.com/6235-7166-large/bugdroid-android.png")
+
+        val urlConnection = url.openConnection() as HttpURLConnection
+        urlConnection.doInput = true
+        urlConnection.connect()
+
+        val inputStream = urlConnection.inputStream
+        val bitMap = BitmapFactory.decodeStream(inputStream)
+        image.setImageBitmap(bitMap)
+
     }
 }
